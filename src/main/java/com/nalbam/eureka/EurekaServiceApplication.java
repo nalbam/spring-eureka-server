4bd2503cf785db4ec67fe08f1dc00d32c0629661
nalbam <me@nalbam.com>
2018-04-23 19:45:35 +0900

    cloud-config

--- src/main/java/com/nalbam/eureka/EurekaServiceApplication.java
+++ src/main/java/com/nalbam/eureka/EurekaServiceApplication.java
@@ -2,9 +2,11 @@ package com.nalbam.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaServiceApplication {
 
